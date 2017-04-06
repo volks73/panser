7353ca71d521d4e082633fea890dc88bc20f3b88
Christopher Field <cfield2@gmail.com>
2017-04-06 18:21:02 -0400

    Copy example from serde_transcode documentation
    The serde_transcode example is copied to create the initial working
    binary.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate serde_json;
+extern crate serde_transcode;
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let input = r#"
+    {
+        "a boolean": true,
+        "an array": [3, 2, 1]
+    }"#;
+    let iter = input.bytes().map(Ok);
+    let mut deserializer = serde_json::Deserializer::from_iter(iter);
+    let mut serializer = serde_json::Serializer::new(io::stdout());
+
+    serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
 }
+
