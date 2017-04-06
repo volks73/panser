38ba62d244b24e87f084552fdcec8eb2a1a63aef
Christopher Field <cfield2@gmail.com>
2017-04-06 18:35:19 -0400

    Add reading from STDIN

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,10 @@ extern crate serde_transcode;
 use std::io;
 
 fn main() {
-    let input = r#"
-    {
-        "a boolean": true,
-        "an array": [3, 2, 1]
-    }"#;
-    let iter = input.bytes().map(Ok);
-    let mut deserializer = serde_json::Deserializer::from_iter(iter);
+    let mut deserializer = serde_json::Deserializer::from_reader(io::stdin());
     let mut serializer = serde_json::Serializer::new(io::stdout());
 
     serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
+    println!("");
 }
 
