f5b40ad6a4750dd8a3058a28e4f63f7db7197ad7
Christopher Field <cfield2@gmail.com>
2017-04-06 20:52:55 -0400

    Add JSON input to MessagePack output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
+extern crate serde;
 extern crate serde_json;
 extern crate serde_transcode;
+extern crate rmp_serde;
 
-use std::io;
+use serde::ser::Serialize;
+use std::io::{self, Write};
 
 fn main() {
-    let mut deserializer = serde_json::Deserializer::from_reader(io::stdin());
-    let mut serializer = serde_json::Serializer::new(io::stdout());
-
-    serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
-    println!("");
+    let mut input = String::new();
+    //let input = "{\"bool\":true}";
+    //let iter = input.bytes().map(Ok);
+    //let mut deserializer = serde_json::Deserializer::from_reader(io::stdin());
+    //let mut deserializer = serde_json::Deserializer::from_iter(iter);
+    io::stdin().read_line(&mut input).unwrap();
+    let value: serde_json::Value = serde_json::from_str(&input).unwrap();
+    let result = value.serialize(&mut rmp_serde::Serializer::new(io::stdout()));
+    println!();
+    match result {
+        Ok(_) => {
+            std::process::exit(0);
+        },
+        Err(e) => {
+            writeln!(&mut std::io::stderr(), "{}", e).expect("Writing to STDERR");
+            std::process::exit(1);
+        }
+    }
 }
 
