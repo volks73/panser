0b16060771345ea801d9991572f7036e764edb8a
Christopher Field <cfield2@gmail.com>
2017-04-09 18:31:54 -0400

    Add `-t,--to` option
    Not implemented, but the option exists in the help menu.

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,130 @@ use panser::{Error, Result};
 use serde::ser::Serialize;
 use std::fs::File;
 use std::io::{self, Cursor, ErrorKind, Read, Write};
-use std::str;
+use std::str::{self, FromStr};
 use std::sync::mpsc;
 use std::thread;
+
+enum ToFormat {
+    Bincode,
+    Bson,
+    Cbor,
+    Hjson,
+    Json,
+    Msgpack,
+    Pickle,
+    Toml,
+    Url,
+    Yaml,
+}
+
+impl FromStr for ToFormat {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            "Bincode" => Ok(ToFormat::Bincode),
+            "bincode" => Ok(ToFormat::Bincode),
+            "BINCODE" => Ok(ToFormat::Bincode),
+            "Bson" => Ok(ToFormat::Bson),
+            "bson" => Ok(ToFormat::Bson),
+            "BSON" => Ok(ToFormat::Bson),
+            "Cbor" => Ok(ToFormat::Cbor),
+            "cbor" => Ok(ToFormat::Cbor),
+            "CBOR" => Ok(ToFormat::Cbor),
+            "Hjson" => Ok(ToFormat::Hjson),
+            "hjson" => Ok(ToFormat::Hjson),
+            "HJSON" => Ok(ToFormat::Hjson),
+            "Json" => Ok(ToFormat::Json),
+            "json" => Ok(ToFormat::Json),
+            "JSON" => Ok(ToFormat::Json),
+            "Msgpack" => Ok(ToFormat::Msgpack),
+            "msgpack" => Ok(ToFormat::Msgpack),
+            "MSGPACK" => Ok(ToFormat::Msgpack),
+            "MessagePack" => Ok(ToFormat::Msgpack),
+            "Pickle" => Ok(ToFormat::Pickle),
+            "pickle" => Ok(ToFormat::Pickle),
+            "PICKLE" => Ok(ToFormat::Pickle),
+            "Toml" => Ok(ToFormat::Toml),
+            "toml" => Ok(ToFormat::Toml),
+            "TOML" => Ok(ToFormat::Toml),
+            "Url" => Ok(ToFormat::Url),
+            "url" => Ok(ToFormat::Url),
+            "URL" => Ok(ToFormat::Url),
+            "Yaml" => Ok(ToFormat::Yaml),
+            "yaml" => Ok(ToFormat::Yaml),
+            "YAML" => Ok(ToFormat::Yaml),
+            _ => Err("No Match")
+        }
+    }
+}
+
+enum FromFormat {
+    Bincode,
+    Bson,
+    Cbor,
+    Envy,
+    Hjson,
+    Json,
+    Msgpack,
+    Pickle,
+    Redis,
+    Toml,
+    Url,
+    Xml,
+    Yaml,
+}
+
+impl FromStr for FromFormat {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s {
+            "Bincode" => Ok(FromFormat::Bincode),
+            "bincode" => Ok(FromFormat::Bincode),
+            "BINCODE" => Ok(FromFormat::Bincode),
+            "Bson" => Ok(FromFormat::Bson),
+            "bson" => Ok(FromFormat::Bson),
+            "BSON" => Ok(FromFormat::Bson),
+            "Cbor" => Ok(FromFormat::Cbor),
+            "cbor" => Ok(FromFormat::Cbor),
+            "CBOR" => Ok(FromFormat::Cbor),
+            "Envy" => Ok(FromFormat::Envy),
+            "envy" => Ok(FromFormat::Envy),
+            "ENVY" => Ok(FromFormat::Envy),
+            "Hjson" => Ok(FromFormat::Hjson),
+            "hjson" => Ok(FromFormat::Hjson),
+            "HJSON" => Ok(FromFormat::Hjson),
+            "Json" => Ok(FromFormat::Json),
+            "json" => Ok(FromFormat::Json),
+            "JSON" => Ok(FromFormat::Json),
+            "Msgpack" => Ok(FromFormat::Msgpack),
+            "msgpack" => Ok(FromFormat::Msgpack),
+            "MSGPACK" => Ok(FromFormat::Msgpack),
+            "MessagePack" => Ok(FromFormat::Msgpack),
+            "Pickle" => Ok(FromFormat::Pickle),
+            "pickle" => Ok(FromFormat::Pickle),
+            "PICKLE" => Ok(FromFormat::Pickle),
+            "Redis" => Ok(FromFormat::Redis),
+            "redis" => Ok(FromFormat::Redis),
+            "REDIS" => Ok(FromFormat::Redis),
+            "Toml" => Ok(FromFormat::Toml),
+            "toml" => Ok(FromFormat::Toml),
+            "TOML" => Ok(FromFormat::Toml),
+            "Url" => Ok(FromFormat::Url),
+            "url" => Ok(FromFormat::Url),
+            "URL" => Ok(FromFormat::Url),
+            "Xml" => Ok(FromFormat::Xml),
+            "xml" => Ok(FromFormat::Xml),
+            "XML" => Ok(FromFormat::Xml),
+            "Yaml" => Ok(FromFormat::Yaml),
+            "yaml" => Ok(FromFormat::Yaml),
+            "YAML" => Ok(FromFormat::Yaml),
+            _ => Err("No Match")
+        }
+    }
+}
+
 
 fn transcode<W: Write>(input: &[u8], output: &mut W, framed: bool) -> Result<()> {
     let value: serde_json::Value = serde_json::from_slice(input)?;
@@ -117,19 +238,31 @@ fn main() {
         .author("Christopher R. Field <cfield2@gmail.com>")
         .about("An application for transcoding serialization formats.") 
         .arg(Arg::with_name("FILE")
-             .help("A file to read as input instead of reading from STDIN. If a file extension exists, then it is used to determine the format of the serialized data contained within the file. If a file extension does not exist, then the '-f,--from' option should be used or JSON is assumed.")
-             .index(1))
+            .help("A file to read as input instead of reading from STDIN. If a file extension exists, then it is used to determine the format of the serialized data contained within the file. If a file extension does not exist, then the '-f,--from' option should be used or JSON is assumed.")
+            .index(1))
         .arg(Arg::with_name("framed-input")
-             .help("Indicates the first four bytes of the input is an unsigned 32-bit integer in Big Endian (Network Order) indicating the total length of the serialized data.")
-             .long("framed-input"))
+            .help("Indicates the first four bytes of the input is an unsigned 32-bit integer in Big Endian (Network Order) indicating the total length of the serialized data.")
+            .long("framed-input"))
         .arg(Arg::with_name("framed-output")
-             .help("Prepends the total length of the serialized data as an unsigned 32-bit integer in Big Endian (Network Order).")
-             .long("framed-output"))
+            .help("Prepends the total length of the serialized data as an unsigned 32-bit integer in Big Endian (Network Order).")
+            .long("framed-output"))
+        .arg(Arg::with_name("from")
+            .help("The input format. [default: JSON]")
+            .long("from")
+            .short("f")
+            .takes_value(true))
         .arg(Arg::with_name("output")
-             .help("A file to write the output instead of writing to STDOUT. If a file extension exists, then it is used to determined the format of the output serialized data. If a file extension does not exist, then the `-t,--to` opton should be used or MessagePack is assumed.")
-             .long("output")
-             .short("o")
-             .takes_value(true))
+            .help("A file to write the output instead of writing to STDOUT. If a file extension exists, then it is used to determined the format of the output serialized data. If a file extension does not exist, then the `-t,--to` opton should be used or MessagePack is assumed.")
+            .long("output")
+            .short("o")
+            .takes_value(true))
+        .arg(Arg::with_name("to")
+            .help("The output format. [values: Bincode, BSON, CBOR, Hjson, JSON, Msgpack, Pickle, TOML, URL, YAML] [default: Msgpack]")
+            .long("to")
+            .short("t")
+            .possible_values(&["Bincode", "BSON", "CBOR", "Hjson", "JSON", "Msgpack", "Pickle", "TOML", "URL", "YAML"])
+            .hide_possible_values(true)
+            .takes_value(true))
         .get_matches();
     let result = run(
         matches.value_of("FILE"), 
