bdaabf098752c9fb3d5de22740493e18aba178c4
Christopher Field <cfield2@gmail.com>
2017-04-08 21:14:20 -0400

    Add framed output implementation
    If the `--framed-output` flag is specified, the total serialized data
    size is prepended to the output as an unsigned 32-bit integer in Big
    Endian (Network Order).

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 
 #[macro_use]
 extern crate clap;
+extern crate byteorder;
 extern crate panser;
 extern crate serde;
 extern crate serde_json;
 extern crate serde_transcode;
 extern crate rmp_serde;
 
+use byteorder::{ByteOrder, BigEndian};
 use clap::{App, Arg};
 use panser::Result;
 use serde::ser::Serialize;
@@ -16,33 +18,41 @@ use std::io::{self, BufReader, Read, Write};
 use std::sync::mpsc;
 use std::thread;
 
-fn transcode(input: &str, output: Option<&str>) -> Result<()> {
-    let out: Box<Write> = output.map_or(Box::new(io::stdout()), |o| {
+fn transcode(input: &str, output: Option<&str>, framed: bool) -> Result<()> {
+    let mut out: Box<Write> = output.map_or(Box::new(io::stdout()), |o| {
         Box::new(File::create(o).unwrap())
     });
     let value: serde_json::Value = serde_json::from_str(input)?;
-    value.serialize(&mut rmp_serde::Serializer::new(out))?;
+    let mut buf = Vec::new();
+    value.serialize(&mut rmp_serde::Serializer::new(&mut buf))?;
+    if framed {
+        let mut frame_length = [0; 4];
+        BigEndian::write_u32(&mut frame_length, buf.len() as u32);
+        out.write(&frame_length)?;
+    }
+    out.write(&buf)?;
+    out.flush()?;
     Ok(())
 }
 
-fn run(input: Option<&str>, output: Option<&str>, suppress_newline: bool) -> Result<()> {
+fn run(input: Option<&str>, output: Option<&str>, suppress_newline: bool, framed_output: bool) -> Result<()> {
     if let Some(i) = input {
-        run_file(i, output)
+        run_file(i, output, framed_output)
     } else {
-        run_stdin(output, suppress_newline)
+        run_stdin(output, suppress_newline, framed_output)
     }
 }
 
-fn run_file(input: &str, output: Option<&str>) -> Result<()> {
+fn run_file(input: &str, output: Option<&str>, framed_output: bool) -> Result<()> {
     let file = File::open(input)?;
     let mut buf_reader = BufReader::new(file);
     let mut buf = String::new();
     buf_reader.read_to_string(&mut buf)?;
-    transcode(&buf, output)?;
+    transcode(&buf, output, framed_output)?;
     Ok(())
 }
 
-fn run_stdin(output: Option<&str>, suppress_newline: bool) -> Result<()> {
+fn run_stdin(output: Option<&str>, suppress_newline: bool, framed_output: bool) -> Result<()> {
     // Reading from STDIN should be conducted on a separate thread since it is blocking.
     let (message_tx, message_rx) = mpsc::channel::<String>();
     thread::spawn(move || {
@@ -62,7 +72,7 @@ fn run_stdin(output: Option<&str>, suppress_newline: bool) -> Result<()> {
     });
     loop {
         if let Ok(input) = message_rx.recv() {
-            transcode(&input, output)?;
+            transcode(&input, output, framed_output)?;
             if suppress_newline {
                 io::stdout().flush()?;
             } else {
@@ -93,10 +103,10 @@ fn main() {
              .help("A file to read as input instead of reading from STDIN. If a file extension exists, then it is used to determine the format of the serialized data contained within the file. If a file extension does not exist, then the '-f,--from' option should be used or JSON is assumed.")
              .index(1))
         .arg(Arg::with_name("framed-input")
-             .help("Indicates the first four bytes of the input is a signed 32-bit integer in Big Endian (Network Order) indicating the total length of the serialized data.")
+             .help("Indicates the first four bytes of the input is an unsigned 32-bit integer in Big Endian (Network Order) indicating the total length of the serialized data.")
              .long("framed-input"))
         .arg(Arg::with_name("framed-output")
-             .help("Prepends the total length of the serialized data as a signed 32-bit integer in Big Endian (Network Order).")
+             .help("Prepends the total length of the serialized data as an unsigned 32-bit integer in Big Endian (Network Order).")
              .long("framed-output"))
         .arg(Arg::with_name("output")
              .help("A file to write the output instead of writing to STDOUT. If a file extension exists, then it is used to determined the format of the output serialized data. If a file extension does not exist, then the `-t,--to` opton should be used or MessagePack is assumed.")
@@ -108,7 +118,12 @@ fn main() {
              .long("suppress-newline")
              .short("n"))
         .get_matches();
-    let result = run(matches.value_of("FILE"), matches.value_of("output"), matches.is_present("suppress-newline"));
+    let result = run(
+        matches.value_of("FILE"), 
+        matches.value_of("output"), 
+        matches.is_present("suppress-newline"), 
+        matches.is_present("framed-output")
+    );
     match result {
         Ok(_) => {
             std::process::exit(0);
